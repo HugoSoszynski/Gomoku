9ef8eb2390e32882ca7a26ae68d3e31d4838db96
Maxime Pillon <maxime.pillon@epitech.eu>
2017-11-28 10:37:29 +0100

    starting the creation of class, Serializer and Core

---
+++ Gomoku/Core.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Gomoku {
+    public class Core {
+
+        private Serializer _serializer;
+
+        public Core() {
+            _serializer = new Serializer();
+        }
+
+        public void Read() {
+            Read();
+        }
+    }
+}
---
+++ Gomoku/Serializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Gomoku {
+    public class Serializer {
+
+        public Serializer() {}
+    }
+}
