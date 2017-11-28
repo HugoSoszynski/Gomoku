b977339f194c830a2d93a3fb6b0c42efaf00bf4e
maxencedcx <maxence.delacroix@epitech.eu>
2017-11-28 13:16:22 +0100

    Serializer update

--- Gomoku/Serializer.cs
+++ Gomoku/Serializer.cs
@@ -3,17 +3,28 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using Gomoku.Commands;
 
-namespace Gomoku {
+namespace Gomoku
+{
     public class Serializer {
 
         public Serializer() {}
 
-        public void Read() {
-
+        public DataCommand Read()
+        {
+            while (true)
+            {
+                var tmp = CommandDictionary.From(Console.ReadLine());
+                if (tmp.CommandType == ECommand.UNKNOWN)
+                    Console.Out.WriteLine("UNKNOWN");
+                else
+                    return tmp;
+            }
         }
 
-        public void Send() {
+        public void Send()
+        {
 
         }
     }
