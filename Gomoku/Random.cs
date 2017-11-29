6f86b6f354705a21d86edc2e1bec45f5aaa95d49
Hugo Soszynski <hugo.soszynski@epitech.eu>
2017-11-29 10:32:35 +0100

    Added Board utils method and random AI for testing

---
+++ Gomoku/Random.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gomoku {
+    public class Random
+        : IAI {
+        public Tuple<uint, uint> MakeMove(Board board) {
+            return board.PossibleMoves().First();
+        }
+    }
+}
