d93d561f6c6836643d0c871de1363f4dd174ceed
maxencedcx <maxence.delacroix@epitech.eu>
2017-12-05 15:06:24 +0100

    Debug + diagonal left

--- Gomoku/Evaluator.cs
+++ Gomoku/Evaluator.cs
@@ -19,33 +19,38 @@ namespace Gomoku
                 return Double.MaxValue;
             if (scores.Item2.Five)
                 return Double.MinValue;
-            return (scores.Item1.Four * 100 + scores.Item1.Three * 5 + scores.Item1.Two * 1) -
-                   (scores.Item2.Four * 100 + scores.Item2.Three * 5 + scores.Item2.Two * 1);
+            double opponentScore = scores.Item2.Four * 80 + scores.Item2.Three * 3 + scores.Item2.Two * 0;
+            double myselfScore = scores.Item1.Four * 100 + scores.Item1.Three * 5 + scores.Item1.Two * 1;
+            double tmp = myselfScore - opponentScore;
+            return tmp;
         }
 
         // Tuple Item1 is score of myself, Item2 is score of opponent
         private Tuple<Score, Score> WinChains(Board board)
         {
-            Tuple<Score, Score> scores = new Tuple<Score, Score>(new Score{Five = false, Four = 0, Three = 0, Two = 0},
-                                                                 new Score{Five = false, Four = 0, Three = 0, Two = 0});
+            Score opponentScore = new Score{Five = false, Four = 0, Three = 0, Two = 0};
+            Score myselfScore = new Score{Five = false, Four = 0, Three = 0, Two = 0};
+            
             for (int x = 0; x < board.Size; x++)
             {
                 for (int y = 0; y < board.Size; y++)
                 {
-                    FulfillScore(NewColStates(board, x, y), scores.Item1, State.Myself);
-                    FulfillScore(NewRowStates(board, x, y), scores.Item1, State.Myself);
-                    FulfillScore(NewDiagStates(board, x, y), scores.Item1, State.Myself);
-                    FulfillScore(NewColStates(board, x, y), scores.Item2, State.Opponent);
-                    FulfillScore(NewRowStates(board, x, y), scores.Item2, State.Opponent);
-                    FulfillScore(NewDiagStates(board, x, y), scores.Item2, State.Opponent);
-                    if (scores.Item1.Five || scores.Item2.Five)
-                        return scores;
+                    FulfillScore(NewColStates(board, x, y), ref myselfScore, State.Myself);
+                    FulfillScore(NewRowStates(board, x, y), ref myselfScore, State.Myself);
+                    FulfillScore(NewDiagRightStates(board, x, y), ref myselfScore, State.Myself);
+                    FulfillScore(NewDiagLeftStates(board, x, y), ref myselfScore, State.Myself);
+                    FulfillScore(NewColStates(board, x, y), ref opponentScore, State.Opponent);
+                    FulfillScore(NewRowStates(board, x, y), ref opponentScore, State.Opponent);
+                    FulfillScore(NewDiagRightStates(board, x, y), ref opponentScore, State.Opponent);
+                    FulfillScore(NewDiagLeftStates(board, x, y), ref opponentScore, State.Opponent);
+                    if (myselfScore.Five || opponentScore.Five)
+                        return new Tuple<Score, Score>(myselfScore, opponentScore);
                 }
             }
-            return scores;
+            return new Tuple<Score, Score>(myselfScore, opponentScore);
         }
 
-        private void FulfillScore(State[] array, Score score, State player)
+        private Score FulfillScore(State[] array, ref Score score, State player)
         {
             var tmp = LongestChain(array, player);
             switch (tmp)
@@ -63,6 +68,7 @@ namespace Gomoku
                     ++score.Two;
                     break;
             }
+            return score;
         }
 
         private State[] NewRowStates(Board board, int x, int y)
@@ -93,7 +99,7 @@ namespace Gomoku
             };
         }
 
-        private State[] NewDiagStates(Board board, int x, int y)
+        private State[] NewDiagRightStates(Board board, int x, int y)
         {
             if (y + 4 >= board.Size || x + 4 >= board.Size) 
                 return null;
@@ -104,6 +110,20 @@ namespace Gomoku
                 board.Map[x + 2, y + 2],
                 board.Map[x + 3, y + 3],
                 board.Map[x + 4, y + 4]
+            };
+        }
+
+        private State[] NewDiagLeftStates(Board board, int x, int y)
+        {
+            if (y - 4 < 0 || x + 4 >= board.Size) 
+                return null;
+            return new []
+            {
+                board.Map[x, y],
+                board.Map[x + 1, y - 1],
+                board.Map[x + 2, y - 2],
+                board.Map[x + 3, y - 3],
+                board.Map[x + 4, y - 4]
             };
         }
 
